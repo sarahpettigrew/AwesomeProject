e92ed2909b47aff0024095e50469b7c2741b3fce
SarahPettigrew <sarahepettigrew@outlook.com>
2022-06-02 01:13:09 -0700

    Added header, description, and list

--- App.js
+++ App.js
@@ -1,41 +1,59 @@
 import React from 'react';
-import {View, Text, FlatList, ScrollView, StyleSheet} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22,
+    paddingTop: 40,
   },
   item: {
+    textAlign: 'center',
     padding: 10,
     fontSize: 18,
     height: 44,
   },
 });
 
+const english_lavendar = '#AA767C';
+const glossy_grape = '#A799B7';
+
 const App = () => {
   return (
-    <ScrollView>
-      <Text>Sarah Pettigrew AD340</Text>
-      <View>
-        <Text>This app is for learning React Native in AD340 Spring 2022</Text>
-      </View>
-      <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-      />
-    </ScrollView>
+    <FlatList
+      style={{backgroundColor: english_lavendar}}
+      ListHeaderComponent={
+        <>
+          <Text
+            style={{
+              fontSize: 30,
+              textAlign: 'center',
+              padding: 10,
+              fontWeight: 'bold',
+              textDecorationLine: 'underline',
+              backgroundColor: english_lavendar,
+            }}>
+            Sarah Pettigrew AD340
+          </Text>
+          <Text
+            style={{
+              padding: 10,
+              fontSize: 22,
+              marginHorizontal: 20,
+              backgroundColor: glossy_grape,
+              borderRadius: 10,
+            }}>
+            This app is for learning React Native while taking AD340 Spring 2022
+          </Text>
+        </>
+      }
+      data={[
+        {key: 'Movies'},
+        {key: 'Traffic Cameras'},
+        {key: 'Traffic Maps'},
+        {key: 'Other'},
+      ]}
+      renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+    />
   );
 };
 
